848bdde1f34a8ed9f7c89d138590201b1d2e17ed
mtmart <mtmartadd@gmail.com>
2021-02-06 18:51:17 +0600

    new txt added on main

--- main.go
+++ main.go
@@ -6,5 +6,8 @@ func main() {
 	
 fmt.Println(" Welcome to mtmart")
 
-fmt.Println("Many Many Thanks to Master Academy for this opurtunaty ")
+fmt.Println("Many Many Thanks to Master Academy for this opportunity ")
+fmt.Println("My name is Muhammad Rezaul Karim")
+fmt.Println("I am 33years old")
+fmt.Println("I am from Chittagong.")
 }
